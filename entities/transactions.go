7c619c1dcd188f32a8d497a16bf431ec32abef9f
Luís Miranda <luistm@gmail.com>
2017-06-18 17:04:33 +0100

    Renames Transaction field to value

--- entities/transactions.go
+++ entities/transactions.go
@@ -14,7 +14,7 @@ var DEBT = "Debt"
 
 // Transaction is a money movement
 type Transaction struct {
-	field           string
+	value           string
 	Description     string
 	TransactionType string
 }
@@ -23,7 +23,7 @@ type Transaction struct {
 func (t *Transaction) New(record Record) *Transaction {
 
 	for i := 0; i < len(record.Record); i++ {
-		t.field = record.Record[i]
+		t.value = record.Record[i]
 		t.Description = record.Record[2]
 
 		// Expense
@@ -44,7 +44,7 @@ func (t *Transaction) New(record Record) *Transaction {
 
 // Value returns the field value parsed for money
 func (t *Transaction) Value() decimal.Decimal {
-	parsedField := strings.Replace(t.field, ".", "", -1)
+	parsedField := strings.Replace(t.value, ".", "", -1)
 	parsedField = strings.Replace(parsedField, ",", ".", -1)
 	// retValue, _ := strconv.ParseFloat(parsedField, 64)
 
