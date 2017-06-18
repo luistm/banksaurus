75638ad15546bb3e549785caaf56720451c073f3
Luís Miranda <luistm@gmail.com>
2017-06-18 21:16:35 +0100

    Validates if string does not starts with the header

--- entities/records.go
+++ entities/records.go
@@ -1,5 +1,7 @@
 package entities
 
+import "strings"
+
 // Record represents a single line of an importend transaction.
 // Data will be imported most likely from a text base format.
 // Therefore a record will be the representation of each of of those lines.
@@ -10,7 +12,8 @@ type Record struct {
 
 // Valid returns a boolean regarding if the record is valid or not
 func (r *Record) Valid() bool {
-	if len(r.Record) == 8 {
+	// TODO: Use regular expression
+	if len(r.Record) == 8 && !strings.HasPrefix(r.Record[0], "Data mov") {
 		return true
 	}
 
