b3855394a41bad94cb7b408f78f3a7175a64a597
Luís Miranda <luistm@gmail.com>
2017-08-27 17:34:54 +0100

    Initial implementation on save category method

--- categories/repository.go
+++ categories/repository.go
@@ -1,1 +1,20 @@
 package categories
+
+import (
+	"database/sql"
+	"errors"
+)
+
+// CategoryRepository allows us the save a read categories from a repository
+type CategoryRepository struct {
+	db *sql.DB
+}
+
+// Save to persist a category
+func (cr *CategoryRepository) Save(c *Category) error {
+
+	// Query goes here...
+	// INSERT INTO CATEGORY VALUE (name);
+
+	return errors.New("Failed to save category")
+}
--- categories/respository_test.go
+++ categories/respository_test.go
@@ -1,2 +1,17 @@
 package categories
 
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCategoryRepositorySave(t *testing.T) {
+	if !testing.Short() {
+		t.Skip()
+	}
+
+	cr := CategoryRepository{}
+	err := cr.Save(&Category{})
+	assert.Error(t, err)
+}
