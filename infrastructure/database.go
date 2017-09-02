7e02f8dae2466eb5f25686dad61f856f8646ad9f
Luís Miranda <luistm@gmail.com>
2017-09-02 15:52:32 +0100

    Adds methods to the database handler

--- infrastructure/database.go
+++ infrastructure/database.go
@@ -7,3 +7,13 @@ type DatabaseHandler struct {
 	path string
 	*sql.DB
 }
+
+// Execute an sql statement
+func (dh *DatabaseHandler) Execute(statement string) error {
+	return nil
+}
+
+// Query fetches data from the database
+func (dh *DatabaseHandler) Query(statement string) (*sql.Rows, error) {
+	return nil, nil
+}
