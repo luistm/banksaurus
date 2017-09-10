c9a395885923b1a6cd61ebd09af2fb71da10b5bb
Luís Miranda <luistm@gmail.com>
2017-09-10 11:24:23 +0100

    Removes legacy code

--- main.go
+++ main.go
@@ -12,137 +12,8 @@ import (
 	"fmt"
 	"log"
 
-	// _ "github.com/mattn/go-sqlite3"
 	flag "github.com/ogier/pflag"
-	"github.com/shopspring/decimal"
-	"github.com/tealeg/xlsx"
 )
-
-var DATABASE_NAME string = "./expensetracker.db"
-var DATABASE_ENGINE = "sqlite3"
-
-func toExcel(value decimal.Decimal, description string) {
-	var file *xlsx.File
-	var sheet *xlsx.Sheet
-	var row *xlsx.Row
-	var cell *xlsx.Cell
-	var err error
-
-	file = xlsx.NewFile()
-	sheet, err = file.AddSheet("Sheet1")
-	if err != nil {
-		fmt.Printf(err.Error())
-	}
-	row = sheet.AddRow()
-	cell = row.AddCell()
-	cell.Value = description
-	cell = row.AddCell()
-	// cell.Value = strconv.FormatFloat(value, 'f', 2, 64)
-	cell.Value = value.String()
-
-	err = file.Save("MyXLSXFile.xlsx")
-	if err != nil {
-		fmt.Printf(err.Error())
-	}
-}
-
-// // Database is the holder for database operations
-// type Database struct {
-// 	db *sql.DB
-// }
-
-// // NewDBConnection provides a new connection to the database
-// func (d *Database) NewDBConnection() error {
-// 	// log.Println("Creating new database connection")
-
-// 	if _, err := os.Stat(DATABASE_NAME); os.IsNotExist(err) && d.db == nil {
-// 		// os.Remove(DATABASE_NAME)
-// 		db, err := sql.Open(DATABASE_ENGINE, DATABASE_NAME)
-// 		if err != nil {
-// 			return err
-// 		}
-
-// 		d.db = db
-
-// 		return nil
-// 	}
-
-// 	if _, err := os.Stat(DATABASE_NAME); os.IsNotExist(err) && d.db != nil {
-// 		log.Fatal("Connection exists, but database file does not... wtf??")
-// 	}
-
-// 	if d.db == nil {
-// 		os.Remove(DATABASE_NAME)
-// 		db, err := sql.Open(DATABASE_ENGINE, DATABASE_NAME)
-// 		if err != nil {
-// 			return err
-// 		}
-
-// 		d.db = db
-
-// 		return nil
-// 	}
-
-// 	return errors.New("Database connection already exists")
-// }
-
-// // CreateExpenseDatabase creates the expense database
-// func (d *Database) CreateExpenseDatabase() error {
-// 	// log.Println("Creating new database")
-
-// 	sqlStmt := `
-// 	create table expenses (id integer not null primary key, description text, value float);
-// 	create table credits (id integer not null primary key, description text, value float);
-// 	delete from expenses;
-// 	delete from expenses;
-// 	`
-// 	d.db.Ping()
-// 	_, err := d.db.Exec(sqlStmt)
-// 	if err != nil {
-// 		// log.Printf("%q: %s\n", err, sqlStmt)
-// 		return err
-// 	}
-
-// 	return nil
-// }
-
-// func (d *Database) Close() {
-// 	d.db.Close()
-// }
-
-// func (d *Database) SaveExpense(value decimal.Decimal, description string) {
-// 	_, err := d.db.Exec("insert into expenses(value, description) values(?, ?)", value, description)
-// 	if err != nil {
-// 		log.Fatal("Error while saving expense: ", err)
-// 	}
-// }
-
-// func (d *Database) SaveCredit(value float64, description string) {
-// 	tx, err := d.db.Begin()
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	stmt, err := tx.Prepare("insert into credits(id, name) values(?, ?)")
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	defer stmt.Close()
-// 	_, err = stmt.Exec()
-// 	if err != nil {
-// 		log.Fatal(err)
-// 	}
-// 	tx.Commit()
-// }
-
-// database := Database{}
-// if err := database.NewDBConnection(); err != nil {
-// 	log.Fatal(err)
-// }
-// defer database.Close()
-// if err := database.CreateExpenseDatabase(); err != nil {
-// 	log.Fatal(err)
-// }
-// database.SaveExpense(decimal.NewFromFloat(1), "descricao")
 
 // CommandCreateCategory handles category creation command
 func CommandCreateCategory(name string) error {
@@ -207,3 +78,31 @@ func main() {
 		fmt.Println(accounts.CurrentBalance().String())
 	}
 }
+
+// var DATABASE_NAME string = "./expensetracker.db"
+// var DATABASE_ENGINE = "sqlite3"
+
+// func toExcel(value decimal.Decimal, description string) {
+// 	var file *xlsx.File
+// 	var sheet *xlsx.Sheet
+// 	var row *xlsx.Row
+// 	var cell *xlsx.Cell
+// 	var err error
+
+// 	file = xlsx.NewFile()
+// 	sheet, err = file.AddSheet("Sheet1")
+// 	if err != nil {
+// 		fmt.Printf(err.Error())
+// 	}
+// 	row = sheet.AddRow()
+// 	cell = row.AddCell()
+// 	cell.Value = description
+// 	cell = row.AddCell()
+// 	// cell.Value = strconv.FormatFloat(value, 'f', 2, 64)
+// 	cell.Value = value.String()
+
+// 	err = file.Save("MyXLSXFile.xlsx")
+// 	if err != nil {
+// 		fmt.Printf(err.Error())
+// 	}
+// }
