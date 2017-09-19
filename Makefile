24d52f10fc48d86dc82308b9653e2ce3413c371c
Luís Miranda <luistm@gmail.com>
2017-09-19 17:15:43 +0100

    Removes integration test because i don't think i need it

--- Makefile
+++ Makefile
@@ -7,15 +7,12 @@ unit-tests:
 coverage-unit:
 	go test ./... -short -cover
 
+system-tests: install
+	go test -run=^TestSystem$
+
 coverage-package:
 	go test $(PACKAGE)  -short -cover -covermode=count -coverprofile=count.out
 	go tool cover -func=count.out
-
-integration-tests:
-	go test ./... -v
-
-system-tests: install
-	go test -run=^TestSystem$
 	
 deps:
 	glide install
--- system_test.go
+++ system_test.go
@@ -1,31 +1,11 @@
 package main
 
 import (
-	"go-cli-bank/categories"
-	"go-cli-bank/infrastructure"
 	"os/exec"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
-
-func TestIntegration(t *testing.T) {
-	if testing.Short() {
-		t.Skip()
-	}
-
-	categoryName := "TestCategory"
-	dbHandler := infrastructure.DatabaseHandler{}
-	cr := categories.CategoryRepository{DBHandler: &dbHandler}
-	i := categories.Interactor{Repository: &cr}
-	c, err := i.NewCategory(categoryName)
-	assert.NoError(t, err)
-	assert.Equal(t, categoryName, c.Name, "Fetches category")
-
-	c, err = i.GetCategory(categoryName)
-	assert.NoError(t, err)
-	assert.Equal(t, categoryName, c.Name, "Fetches category")
-}
 
 func TestSystem(t *testing.T) {
 	if testing.Short() {
@@ -51,10 +31,10 @@ func TestSystem(t *testing.T) {
 			errorExpected: false,
 		},
 		{
-			name:     "Create category",
-			command:  []string{"new", "category", "testCategory"},
-			expected: "Created category 'testCategory'",
-		errorExpected: true,
+			name:          "Create category",
+			command:       []string{"new", "category", "testCategory"},
+			expected:      "Created category 'testCategory'",
+			errorExpected: true,
 		},
 	}
 
