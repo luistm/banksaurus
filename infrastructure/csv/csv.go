2281071df623be56558c4be12a0f8d1bfaa92374
Luís Miranda <luistm@gmail.com>
2017-10-25 19:04:26 +0100

    Refactors csv storage

---
+++ infrastructure/csv/csv.go
@@ -0,0 +1,45 @@
+package csv
+
+import (
+	"encoding/csv"
+	"os"
+
+	"github.com/luistm/go-bank-cli/infrastructure"
+)
+
+// New opens and returns a file handler for a CSV file
+func New(inputFilePath string) (infrastructure.CSVStorage, error) {
+
+	// TODO: Check if path exists and if it is a file
+	_, err := os.Stat(inputFilePath)
+	if err != nil {
+		return nil, err
+	}
+
+	file, err := os.Open(inputFilePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	f := &csvfile{file: file}
+
+	return f, nil
+}
+
+// File represents the content of CSV formated file
+type csvfile struct {
+	file *os.File
+}
+
+// Close to disconnect with associated file
+func (c *csvfile) Close() error {
+	return c.file.Close()
+}
+
+// GetAll returns all lines in the file
+func (c *csvfile) GetAll() {
+	reader := csv.NewReader(c.file)
+	reader.Comma = ';'
+	reader.FieldsPerRecord = -1
+}
--- infrastructure/infrastructure.go
+++ infrastructure/infrastructure.go
@@ -2,8 +2,13 @@ package infrastructure
 
 import "github.com/luistm/go-bank-cli/entities"
 
-// SQLStorage ...
+// SQLStorage for handling SQL databases
 type SQLStorage interface {
 	entities.SQLDatabaseHandler
+	Close() error
+}
+
+// CSVStorage for handling CSV files
+type CSVStorage interface {
 	Close() error
 }
