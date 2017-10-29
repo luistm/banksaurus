e96667991c566df7eba1a7a7217eac6a14375495
Luís Miranda <luistm@gmail.com>
2017-10-29 17:14:31 +0000

    Comments code which needs to be fixed

--- bank/reports/repository.go
+++ bank/reports/repository.go
@@ -1,11 +1,5 @@
 package reports
 
-import (
-	"fmt"
-
-	"github.com/shopspring/decimal"
-)
-
 // CSVHandler to handle csv files
 type CSVHandler interface {
 	GetAll() ([][]string, error)
@@ -18,47 +12,47 @@ type repository struct {
 // ParseAccountMovements imports data from a data source
 func (r *repository) GetAll() error {
 
-	fileRecords, err := r.storage.GetAll()
-	if err != nil {
-		return err
-	}
+	// fileRecords, err := r.storage.GetAll()
+	// if err != nil {
+	// 	return err
+	// }
 
-	var report map[string]decimal.Decimal
-	report = make(map[string]decimal.Decimal)
-	var credit decimal.Decimal
-	var expense decimal.Decimal
+	// var report map[string]decimal.Decimal
+	// report = make(map[string]decimal.Decimal)
+	// var credit decimal.Decimal
+	// var expense decimal.Decimal
 
-	// Read all transactions
-	for lineCount, record := range fileRecords {
+	// // Read all transactions
+	// for lineCount, record := range fileRecords {
 
-		r := transactions.Record{Record: record}
+	// 	r := transactions.Record{Record: record}
 
-		if !r.Valid() || lineCount < 4 {
-			continue
-		}
+	// 	if !r.Valid() || lineCount < 4 {
+	// 		continue
+	// 	}
 
-		t := transactions.Transaction{}
-		transaction := t.New(r)
+	// 	t := transactions.Transaction{}
+	// 	transaction := t.New(r)
 
-		// descriptions.New(transaction.Description)
+	// 	// descriptions.New(transaction.Description)
 
-		if transaction.IsFromThisMonth() {
-			report[transaction.Description] = report[transaction.Description].Add(transaction.Value())
-			if transaction.IsDebt() {
-				expense = expense.Add(transaction.Value())
-			} else {
-				credit = credit.Add(transaction.Value())
-			}
-		}
+	// 	if transaction.IsFromThisMonth() {
+	// 		report[transaction.Description] = report[transaction.Description].Add(transaction.Value())
+	// 		if transaction.IsDebt() {
+	// 			expense = expense.Add(transaction.Value())
+	// 		} else {
+	// 			credit = credit.Add(transaction.Value())
+	// 		}
+	// 	}
 
-	}
+	// }
 
-	for transactionDescription, transactionValue := range report {
-		fmt.Printf("%24s %8s \n", transactionDescription, transactionValue.String())
-	}
+	// for transactionDescription, transactionValue := range report {
+	// 	fmt.Printf("%24s %8s \n", transactionDescription, transactionValue.String())
+	// }
 
-	fmt.Println("Expense is ", expense.String())
-	fmt.Println("Credit is ", credit.String())
+	// fmt.Println("Expense is ", expense.String())
+	// fmt.Println("Credit is ", credit.String())
 
 	return nil
 
