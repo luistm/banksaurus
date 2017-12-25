1e59be05054d54ad0b82d824c912ab00ec89f889
Luís Miranda <luistm@gmail.com>
2017-12-25 17:01:26 +0000

    Refactors sellers command

--- cmd/bankcli/commands/command_seller.go
+++ cmd/bankcli/commands/command_seller.go
@@ -14,76 +14,27 @@ func (s *Seller) Execute(arguments map[string]interface{}) *Response {
 	var out string
 	var err error
 
-	if arguments["seller"].(bool) && arguments["new"].(bool) {
-		out, err = s.createSellerHandler(arguments["<name>"].(string))
-	}
-
-	if arguments["seller"].(bool) && arguments["show"].(bool) {
-		err = s.showSellersHandler()
-	}
-
-	if arguments["seller"].(bool) && arguments["change"].(bool) {
-		out, err = s.sellerChangePrettyName(
-			arguments["<id>"].(string),
-			arguments["<name>"].(string),
-		)
-	}
-
-	return &Response{err: err, output: out}
-}
-
-func (s *Seller) createSellerHandler(name string) (string, error) {
-	var out string
-
 	dbName, dbPath := configurations.GetDatabasePath()
 	SQLStorage, err := sqlite.New(dbPath, dbName, false)
 	if err != nil {
-		return out, err
+		return &Response{err: err, output: out}
 	}
 	defer SQLStorage.Close()
-
-	sellersInteractor := sellers.NewInteractor(SQLStorage, nil)
-	_, err = sellersInteractor.Create(name)
-	if err != nil {
-		return out, err
-	}
-
-	return out, nil
-}
-
-func (s *Seller) showSellersHandler() error {
-	dbName, dbPath := configurations.GetDatabasePath()
-	SQLStorage, err := sqlite.New(dbPath, dbName, false)
-	if err != nil {
-		return err
-	}
-	defer SQLStorage.Close()
-
 	presenter := &CLIPresenter{}
 
 	sellersInteractor := sellers.NewInteractor(SQLStorage, presenter)
-	err = sellersInteractor.GetAll()
-	if err != nil {
-		return err
+
+	if arguments["seller"].(bool) && arguments["new"].(bool) {
+		_, err = sellersInteractor.Create(arguments["<name>"].(string))
 	}
 
-	return nil
-}
-
-func (s *Seller) sellerChangePrettyName(sellerID string, name string) (string, error) {
-	var out string
-	dbName, dbPath := configurations.GetDatabasePath()
-	SQLStorage, err := sqlite.New(dbPath, dbName, false)
-	if err != nil {
-		return out, err
-	}
-	defer SQLStorage.Close()
-
-	sellersInteractor := sellers.NewInteractor(SQLStorage, nil)
-	err = sellersInteractor.Update(sellerID, name)
-	if err != nil {
-		return out, err
+	if arguments["seller"].(bool) && arguments["show"].(bool) {
+		err = sellersInteractor.GetAll()
 	}
 
-	return out, nil
+	if arguments["seller"].(bool) && arguments["change"].(bool) {
+		err = sellersInteractor.Update(arguments["<id>"].(string), arguments["<name>"].(string))
+	}
+
+	return &Response{err: err, output: out}
 }
