1f34ffee48766ad9e9ecda6b99ba8467a4b3121e
Luís Miranda <luistm@gmail.com>
2018-01-13 14:35:49 +0000

    Tweaks to tests

--- cmd/bankcli/commands/presenters_test.go
+++ cmd/bankcli/commands/presenters_test.go
@@ -12,18 +12,20 @@ import (
 func TestUnitCLIPresenterPresent(t *testing.T) {
 
 	testCases := []struct {
-		name     string
-		output   error
-		withMock bool
+		name         string
+		output       error
+		withMock     bool
+		entityOutput string
 	}{
 		{
 			name:   "Returns error if output pipe is not defined",
 			output: errOutputPipeUndefined,
 		},
 		{
-			name:     "Returns expected output in pipe",
-			output:   nil,
-			withMock: true,
+			name:         "Returns expected output in pipe",
+			output:       nil,
+			withMock:     true,
+			entityOutput: "write this to output pipe",
 		},
 	}
 
@@ -34,14 +36,13 @@ func TestUnitCLIPresenterPresent(t *testing.T) {
 		var ioWriterMock bytes.Buffer
 		if tc.withMock {
 			p.output = &ioWriterMock
-
-			em.On("String").Return("write this to output pipe")
+			em.On("String").Return(tc.entityOutput)
 		}
 
 		err := p.Present(em)
 
 		if tc.withMock {
-			testkit.AssertEqual(t, "write this to output pipe\n", ioWriterMock.String())
+			testkit.AssertEqual(t, tc.entityOutput+"\n", ioWriterMock.String())
 		}
 		testkit.AssertEqual(t, tc.output, err)
 	}
