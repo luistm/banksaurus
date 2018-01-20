d509064c98fce88cdf790cfa14a9c9ce1cdfba31
Luís Miranda <luistm@gmail.com>
2018-01-20 08:33:25 +0000

    Improves testkit assert output

--- elib/testkit/testkit.go
+++ elib/testkit/testkit.go
@@ -10,6 +10,6 @@ import (
 // AssertEqual asserts that two values are equal
 func AssertEqual(t *testing.T, expected interface{}, got interface{}) {
 	if !reflect.DeepEqual(expected, got) {
-		t.Errorf("Expected '%v', got '%v'", expected, got)
+		t.Errorf("\n\n---- Expected:\n%v\n++++ Got:\n%v\n", expected, got)
 	}
 }
