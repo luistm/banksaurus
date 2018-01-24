54e4414a58e9ed79fc644f8522050828570f3572
Luís Miranda <luistm@gmail.com>
2018-01-24 17:22:56 +0000

    Revert "Removes option to create database in memory"
    This reverts commit b1470861dd0b5fd7cf4b18807dc6adb069cc9303.

--- cmd/bankcli/commands/command_load.go
+++ cmd/bankcli/commands/command_load.go
@@ -25,7 +25,7 @@ func (l *Load) loadFile(inputFilePath string) error {
 	defer CSVStorage.Close()
 
 	dbName, dbPath := configurations.DatabasePath()
-	SQLStorage, err := sqlite.New(dbPath, dbName)
+	SQLStorage, err := sqlite.New(dbPath, dbName, false)
 	if err != nil {
 		return err
 	}
