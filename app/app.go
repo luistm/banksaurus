4aed58ab992d1136a09e09775740810c826c6368
Luís Miranda <luistm@gmail.com>
2018-05-26 12:49:08 +0100

    Application instance receives the configuration path

--- app/app.go
+++ app/app.go
@@ -3,6 +3,11 @@ package app
 import (
 	"os"
 
+	"strings"
+
+	"path"
+	"path/filepath"
+
 	"github.com/luistm/banksaurus/configurations"
 )
 
@@ -12,11 +17,21 @@ const (
 )
 
 // New creates an instance of App in the specified path.
-// It should receive the path to the configuration file.
-func New(configuration string) (*App, error) {
-	// TODO: Validate the path to the configuration file.
-	application :=  &App{}
-	err := application.Init()
+// It should receive the full path to the configuration file.
+func New(configurationPath string) (*App, error) {
+
+	err := ValidatePath(configurationPath)
+	if err != nil {
+		return &App{}, err
+	}
+
+	projectPath, err := buildProjectPath(filepath.Dir(configurationPath))
+	if err != nil {
+		return &App{}, err
+	}
+
+	application := &App{projectPath}
+	err = application.Init()
 	if err != nil {
 		return &App{}, err
 	}
@@ -24,8 +39,62 @@ func New(configuration string) (*App, error) {
 	return application, nil
 }
 
+// A project must have in the project path
+// - /configurations
+// - /cmd
+// - /infrastructure
+func buildProjectPath(pth string) (string, error) {
+
+	p := path.Join(pth, "..")
+
+	err := ValidatePath(path.Join(p, "/configurations"))
+	if err != nil {
+		return "", ErrInvalidAppStructure
+	}
+
+	err = ValidatePath(path.Join(p, "/cmd"))
+	if err != nil {
+		return "", ErrInvalidAppStructure
+	}
+
+	err = ValidatePath(path.Join(p, "/infrastructure"))
+	if err != nil {
+		return "", ErrInvalidAppStructure
+	}
+
+	return p, nil
+}
+
 // App represents an application
-type App struct{}
+type App struct {
+	ProjectPath string
+}
+
+// ValidatePath checks if a path is valid
+// A path is considered valid if:
+// a) the path exists
+// b) it's not a file
+// If a path is valid the return is nil
+func ValidatePath(path string) error {
+
+	if !strings.HasPrefix(path, "/") {
+		return ErrMalformedPath
+	}
+
+	_, err := os.Stat(path)
+	if err != nil && os.IsNotExist(err) {
+		return ErrPathDoesNotExist
+	}
+	if err != nil {
+		return err
+	}
+
+	//if !fileInfo.IsDir() {
+	//	return ErrPathIsFile
+	//}
+
+	return nil
+}
 
 // Init initializes the application
 func (a *App) Init() error {
---
+++ app/app_test.go
@@ -0,0 +1,105 @@
+package app_test
+
+import (
+	"testing"
+
+	"os"
+	"path"
+
+	"github.com/luistm/banksaurus/app"
+	"github.com/luistm/testkit"
+)
+
+func TestUnitNewApp(t *testing.T) {
+
+	// Try to create app in existing directory, but invalid project path
+	// Try to create app with non existing configuration file
+
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	testCase := []struct {
+		name                  string
+		configurationFilePath string
+		expectedApp           *app.App
+		expectedErr           string
+	}{
+		{
+			name: "Creates app",
+			configurationFilePath: path.Join(pwd, "..", "/configurations/test_conf.json"),
+			expectedApp:           &app.App{ProjectPath: path.Join(pwd, "/..")},
+		},
+		{
+			name: "Creates app with non exiting directory",
+			configurationFilePath: "/ThisDirectoryDoesNotExist",
+			expectedApp:           &app.App{},
+			expectedErr:           app.ErrPathDoesNotExist.Error(),
+		},
+		{
+			name: "Creates app with non exiting configuration file",
+			configurationFilePath: path.Join(pwd, "..", "/configurations/t_conf.json"),
+			expectedApp:           &app.App{},
+			expectedErr:           app.ErrPathDoesNotExist.Error(),
+		},
+		{
+			name: "Creates app in invalid project structure",
+			configurationFilePath: "/tmp",
+			expectedApp:           &app.App{},
+			expectedErr:           app.ErrInvalidAppStructure.Error(),
+		},
+	}
+
+	for _, tc := range testCase {
+		t.Run(tc.name, func(t *testing.T) {
+			application, err := app.New(tc.configurationFilePath)
+
+			if tc.expectedErr != "" {
+				testkit.AssertEqual(t, tc.expectedErr, err.Error())
+			}
+			testkit.AssertEqual(t, tc.expectedApp, application)
+		})
+	}
+}
+
+func TestUnitValidatePath(t *testing.T) {
+
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	testCases := []struct {
+		name   string
+		input  string
+		errMsg string
+	}{
+		{
+			name:   "Returns error if path does not exist",
+			input:  path.Join(pwd, "..", "/ThisPathDoesNotExist"),
+			errMsg: app.ErrPathDoesNotExist.Error(),
+		},
+		//{
+		//	name:   "Returns error if path is file",
+		//	input:  path.Join(pwd, "..", "/app/app.go"),
+		//	errMsg: app.ErrPathIsFile.Error(),
+		//},
+		{
+			name:   "Returns error if path does not have prefix '/'",
+			input:  "app/app.go",
+			errMsg: app.ErrMalformedPath.Error(),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := app.ValidatePath(tc.input)
+
+			// Error is expected
+			if tc.errMsg != "" {
+				testkit.AssertEqual(t, tc.errMsg, err.Error())
+			}
+		})
+	}
+}
