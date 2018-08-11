b536e54ad46f5571122dfe1b95fadf89b22962ef
Luís Miranda <luistm@gmail.com>
2018-08-11 03:47:28 +0100

    Adds test to check if it does not return error

--- next/listsellers/interactor_test.go
+++ next/listsellers/interactor_test.go
@@ -4,12 +4,24 @@ import (
 	"github.com/luistm/banksaurus/next/listsellers"
 	"github.com/luistm/testkit"
 	"testing"
+	"github.com/luistm/banksaurus/next/entity/seller"
 )
+
+type sellerRepository struct{}
+
+func (*sellerRepository) GetAll() []*seller.Entity {
+	panic("implement me")
+}
 
 func TestUnitNewInteractor(t *testing.T) {
 
 	t.Run("Returns error if sellers repository undefined", func(t *testing.T) {
 		_, err := listsellers.NewInteractor(nil)
 		testkit.AssertEqual(t, listsellers.ErrSellersRepositoryUndefined, err)
+	})
+
+	t.Run("Does not return error if sellers repository is defined", func(t *testing.T){
+		_, err := listsellers.NewInteractor(&sellerRepository{})
+		testkit.AssertIsNil(t, err)
 	})
 }
