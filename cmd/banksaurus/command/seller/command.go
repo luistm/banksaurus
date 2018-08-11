3f7cbc8c21d8318ec63f566fbbbd151b75bc34e8
Luís Miranda <luistm@gmail.com>
2018-08-11 15:09:22 +0100

    Initial wiring of seller show command

--- cmd/banksaurus/command/seller/command.go
+++ cmd/banksaurus/command/seller/command.go
@@ -1,5 +1,12 @@
 package seller
 
+import (
+	"github.com/luistm/banksaurus/next/application/infrastructure/relational"
+
+	"github.com/luistm/banksaurus/next/application/adapter/sqlite"
+	"github.com/luistm/banksaurus/next/listsellers"
+)
+
 // Command seller
 type Command struct{}
 
@@ -11,7 +18,26 @@ func (s *Command) Execute(arguments map[string]interface{}) error {
 	}
 
 	if arguments["seller"].(bool) && arguments["show"].(bool) {
-		panic("seller show not implemented")
+
+		db, err := relational.NewDatabase()
+		if err != nil {
+			return err
+		}
+
+		sr, err := sqlite.NewSellerRepository(db)
+		if err != nil {
+			return err
+		}
+
+		i, err := listsellers.NewInteractor(sr, nil)
+		if err != nil {
+			return err
+		}
+
+		err = i.Execute()
+		if err != nil {
+			return err
+		}
 	}
 
 	if arguments["seller"].(bool) && arguments["change"].(bool) {
