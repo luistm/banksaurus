5c0a10a122cd7723a700f0443c5d170e3a1dcf0a
Luís Miranda <luistm@gmail.com>
2018-08-22 20:41:07 +0100

    Adds account repository

---
+++ cmd/bscli/adapter/accountgateway/repository.go
@@ -0,0 +1,29 @@
+package accountgateway
+
+import (
+	"database/sql"
+	"github.com/luistm/banksaurus/account"
+	"github.com/luistm/banksaurus/money"
+	"github.com/pkg/errors"
+)
+
+// ErrDatabaseUndefined ...
+var ErrDatabaseUndefined = errors.New("database is undefined")
+
+// NewRepository ...
+func NewRepository(db *sql.DB) (*Repository, error) {
+	if db == nil {
+		return &Repository{db}, ErrDatabaseUndefined
+	}
+	return &Repository{db}, nil
+}
+
+// Repository for account
+type Repository struct {
+	db *sql.DB
+}
+
+// New creates a new account entity
+func (r *Repository) New(*money.Money) (*account.Entity, error) {
+	return &account.Entity{}, nil
+}
