fd3c6025f1d7eecc791314f7b34936db1a28c886
Luís Miranda <luistm@gmail.com>
2018-09-02 18:22:09 +0100

    Account must have an ID

--- account/entity.go
+++ account/entity.go
@@ -1,18 +1,42 @@
 package account
 
-import "github.com/luistm/banksaurus/money"
+import (
+	"errors"
+	"github.com/luistm/banksaurus/money"
+)
+
+var (
+	// ErrInvalidID ...
+	ErrInvalidID = errors.New("invalid error")
+
+	// ErrInvalidBalance ...
+	ErrInvalidBalance = errors.New("balance is undefined")
+)
 
 // New creates a new instance of account
-func New(balance *money.Money) (*Entity, error) {
-	return &Entity{balance}, nil
+func New(id string, balance *money.Money) (*Entity, error) {
+	if id == "" {
+		return &Entity{}, ErrInvalidID
+	}
+
+	if balance == nil {
+		return &Entity{}, ErrInvalidBalance
+	}
+	return &Entity{id, balance}, nil
 }
 
 // Entity account
 type Entity struct {
+	id      string
 	balance *money.Money
 }
 
 // Balance for the account
 func (m *Entity) Balance() *money.Money {
 	return m.balance
+}
+
+// ID ...
+func (m *Entity) ID() string {
+	return m.id
 }
--- account/entity_test.go
+++ account/entity_test.go
@@ -7,13 +7,25 @@ import (
 	"testing"
 )
 
+func TestUnitAccountNew(t *testing.T) {
+	t.Run("Returns error if invalid ID", func(t *testing.T) {
+		_, err := account.New("", nil)
+		testkit.AssertEqual(t, account.ErrInvalidID, err)
+	})
+
+	t.Run("Returns error if balance undefined", func(t *testing.T) {
+		_, err := account.New("AccountID", nil)
+		testkit.AssertEqual(t, account.ErrInvalidBalance, err)
+	})
+}
+
 func TestUnitAccountBalance(t *testing.T) {
 
-	t.Run("An account has balance", func(t *testing.T) {
+	t.Run("An account must have balance", func(t *testing.T) {
 		initialBalance, err := money.NewMoney(12345)
 		testkit.AssertIsNil(t, err)
 
-		acc, err := account.New(initialBalance)
+		acc, err := account.New("AccountID", initialBalance)
 		testkit.AssertIsNil(t, err)
 
 		testkit.AssertEqual(t, initialBalance, acc.Balance())
